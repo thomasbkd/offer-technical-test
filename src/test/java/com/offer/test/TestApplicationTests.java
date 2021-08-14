ceb4f1dcb6b544d7933b8ce1d83b2bd1fce3d9dd
Thomas BURKHARD <thomas.burkhard57@gmail.com>
2021-08-14 16:14:13 +0200

    add spring initializr files

---
+++ src/test/java/com/offer/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.offer.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
