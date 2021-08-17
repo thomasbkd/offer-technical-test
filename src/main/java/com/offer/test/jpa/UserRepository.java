76bf95d7dcd60fd5b57440019c458963ee505c49
Thomas BURKHARD <thomas.burkhard57@gmail.com>
2021-08-17 18:38:41 +0200

    add javadoc on repository

--- src/main/java/com/offer/test/jpa/UserRepository.java
+++ src/main/java/com/offer/test/jpa/UserRepository.java
@@ -5,8 +5,18 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
+/**
+ * This interface provides all the needed methods to interact with the data source, especially via its mother interface
+ * <code>JpaRepository</code>.
+ * @see JpaRepository
+ */
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    /**
+     * Gets all the users in the data source with the same username as given, and returns them in a list.
+     * @param username the username to find.
+     * @return a list of the users in the data source with the same username as given.
+     * @see JpaRepository
+     */
     List<User> findAllByUsername(String username);
-
 }
